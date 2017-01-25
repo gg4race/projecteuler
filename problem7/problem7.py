f0570b446c89f823bbe9371198345991d99cc4d0
Grace Gu <0.grace.gu.0@gmail.com>
2017-01-24 20:18:51 -0800

    problem 7

--- problem7/problem7.py
+++ problem7/problem7.py
@@ -1,5 +1,26 @@
 def main():
-    pass
+
+    #find 10001st prime
+
+    def isPrime(num, factors):
+        isPrime = True
+        #check if is prime by dividing by primes smaller than itself
+        for factor in factors:
+            if num % factor == 0:
+                isPrime = False
+                break
+        return isPrime
+
+
+    primes = [2]
+    j = 2
+
+    while len(primes) <= 10001:
+        if (isPrime(j, primes)):
+            primes.append(j)
+        j += 1
+
+    print(primes[-1])
 
 
 if __name__ == '__main__':
