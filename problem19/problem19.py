28498276ae3a6e0eb852341e40399fec77588f0d
Grace Gu <0.grace.gu.0@gmail.com>
2017-01-31 22:19:31 -0800

    problems up to about 20

--- problem19/problem19.py
+++ problem19/problem19.py
@@ -1,21 +1,34 @@
 def main():
-    days1 = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    days2 = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    counter = 0
+    sundays = 0
 
-    totalDays = 0
+    def findTotalDays(year, month):
+        if month == 2:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    return 29
+                else:
+                    return 28
+            else:
+                if year % 4 == 0:
+                    return 29
+                else:
+                    return 28
+        elif month in (1, 3, 5, 7, 8, 10, 12):
+            return 31
+        elif month in (4, 6, 9, 11):
+            return 30
+
     for year in range(1901, 2001):
-        if year % 100 == 0:
-            if year % 400 == 0:
-                totalDays += sum(days2)
-            else:
-                totalDays += sum(days1)
-        else:
-            if year % 4 == 0:
-                totalDays += sum(days2)
-            else:
-                totalDays += sum(days1)
+        for month in range(1, 13):
+            totalDays = findTotalDays(year, month)
+            for day in range(1, totalDays + 1):
+                if day == 1:
+                    if counter % 7 == 0:
+                        sundays += 1
+                counter += 1
 
-    print(totalDays)
+    print(sundays)
 
 
 if __name__ == '__main__':
